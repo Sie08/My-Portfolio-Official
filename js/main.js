7194d15042e907c437f3b7b11d545cb2a0987d56
selsieee <87237513+Sie08@users.noreply.github.com>
2026-04-26 11:55:11 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -53,3 +53,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menuToggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  }
+});
